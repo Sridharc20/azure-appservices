d857f3ccb35f2fb282419a6bf5a9cbfe444159a6
sridharc20 <sridharc20@gmail.com>
2020-04-21 22:19:26 +0530

    added function call

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import requests
 
 
 app = Flask(__name__)
@@ -7,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route('/<name>')
+def home(name):
+    return requests.get("https://pythonfunctionapp2020.azurewebsites.net/api/HttpTrigger1?code=0x3ACUo0kOvRPm/tUs9U/9VNBdDe4bdx1YL/CusgkSIGgVobr37ERQ==&name="+name)
+
